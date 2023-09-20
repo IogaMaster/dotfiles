6b0ae6d21d4d2b7d0d52f6835c29d069a3872de0
IogaMaster <iogamastercode@gmail.com>
2023-09-19 18:21:38 -0600

    refactor: :recycle: Garbage collect once a week

--- modules/system/nix/default.nix
+++ modules/system/nix/default.nix
@@ -46,7 +46,7 @@ in {
       gc = {
         automatic = true;
         dates = "weekly";
-        options = "--delete-older-than 30d";
+        options = "--delete-older-than 7d";
       };
 
       # flake-utils-plus
