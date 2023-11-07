3a58d451ad4bb613e18777d28589348d4b8bf6d3
IogaMaster <iogamastercode@gmail.com>
2023-11-07 15:49:45 -0700

    feat: add some arion deployments

---
+++ modules/nixos/services/arion/jellyfin/default.nix
@@ -0,0 +1,32 @@
+{
+  options,
+  config,
+  lib,
+  ...
+}:
+with lib;
+with lib.internal; let
+  cfg = config.services.arion.jellyfin;
+in {
+  options.services.arion.jellyfin = with types; {
+    enable = mkBoolOpt false "Enable jellyfin";
+  };
+
+  config = mkIf cfg.enable {
+    virtualisation.arion.enable = true;
+    virtualisation.arion.projects.jellyfin.settings = {
+      project.name = "jellyfin";
+      services.jellyfin = {
+        service.image = "jellyfin/jellyfin";
+        service.ports = [
+          "8096:8096"
+        ];
+        service.volumes = [
+          "/home/${config.user.name}/.local/share/jellyfin/config:/config"
+          "/home/${config.user.name}/.local/share/jellyfin/cache:/cache"
+          "/home/${config.user.name}/.local/share/jellyfin/media:/media"
+        ];
+      };
+    };
+  };
+}
