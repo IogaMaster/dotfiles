4b660a736df8424e50d9432a66227b10aa8c3449
IogaMaster <iogamastercode@gmail.com>
2023-11-09 12:37:15 -0700

    fix: Use nixos-anywhere disk-config.nix

--- disk-config.nix
+++ disk-config.nix
@@ -1,21 +1,51 @@
+# Example to create a bios compatible gpt partition
 {lib, ...}: {
   disko.devices = {
-    disk.main = {
+    disk.disk1 = {
       device = lib.mkDefault "/dev/sda";
       type = "disk";
       content = {
         type = "gpt";
         partitions = {
           boot = {
+            name = "boot";
             size = "1M";
-            type = "EF02"; # for grub MBR
+            type = "EF02";
+          };
+          esp = {
+            name = "ESP";
+            size = "500M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+            };
           };
           root = {
+            name = "root";
             size = "100%";
+            content = {
+              type = "lvm_pv";
+              vg = "pool";
+            };
+          };
+        };
+      };
+    };
+    lvm_vg = {
+      pool = {
+        type = "lvm_vg";
+        lvs = {
+          root = {
+            size = "100%FREE";
             content = {
               type = "filesystem";
               format = "ext4";
               mountpoint = "/";
+              mountOptions = [
+                "defaults"
+              ];
             };
           };
         };
