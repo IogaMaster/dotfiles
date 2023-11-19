b6b94ac480d7676907be0066c7536784c4e32e7b
IogaMaster <iogamastercode@gmail.com>
2023-11-19 15:22:01 -0700

    feat: add template descriptions

--- templates/default.nix
+++ templates/default.nix
@@ -1,17 +1,22 @@
 _: {
   lib = {
+    description = "Library module template";
     path = ./lib;
   };
   module = {
+    description = "Module template";
     path = ./module;
   };
   overlay = {
+    description = "Overlay template";
     path = ./overlay;
   };
   system = {
+    description = "System template";
     path = ./system;
   };
   home = {
+    description = "Home template";
     path = ./home;
   };
 }
