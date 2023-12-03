6e3ac201cbbd93d4f81c00bfd06c05fae900b36c
IogaMaster <iogamastercode@gmail.com>
2023-12-02 22:30:15 -0700

    temp: downgrade nvidia driver due to steam break

--- modules/nixos/hardware/nvidia/default.nix
+++ modules/nixos/hardware/nvidia/default.nix
@@ -15,7 +15,9 @@ in {
   config = mkIf cfg.enable {
     services.xserver.videoDrivers = ["nvidia"];
     hardware.nvidia.modesetting.enable = true;
-    hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+    # TODO: production -> stable | when https://github.com/NixOS/nixpkgs/pull/268499 is merged
+    hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.production;
 
     environment.variables = {
       CUDA_CACHE_PATH = "$XDG_CACHE_HOME/nv";
