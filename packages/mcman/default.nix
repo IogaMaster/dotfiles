9468f94a6ef0c5a0f26f3fc7422d32d32d406124
IogaMaster <iogamastercode@gmail.com>
2023-12-05 16:48:47 -0700

    feat: update mcman to unstable

--- packages/mcman/default.nix
+++ packages/mcman/default.nix
@@ -1,29 +1,28 @@
-{
-  lib,
-  rustPlatform,
-  fetchFromGitHub,
-  pkg-config,
-  bzip2,
-  zstd,
-  stdenv,
-  darwin,
+{ lib
+, rustPlatform
+, fetchFromGitHub
+, pkg-config
+, bzip2
+, zstd
+, stdenv
+, darwin
 }:
+
 rustPlatform.buildRustPackage rec {
   pname = "mcman";
-  version = "0.3.0";
+  version = "unstable-2023-11-23";
 
   src = fetchFromGitHub {
     owner = "ParadigmMC";
     repo = "mcman";
-    rev = version;
-    hash = "sha256-w8wIVpG3Uvx6Vdzyiu4gJjjx4smv+x18XWNEyPL/d6s=";
+    rev = "20180f9690593e3ca4663cb4e2771a750757b6ca";
+    hash = "sha256-OGIAlm0wvDWcheT6G/hoqTvgDOPUBQQd7LCHu1OQupM=";
   };
 
   cargoLock = {
     lockFile = ./Cargo.lock;
     outputHashes = {
-      "java-properties-1.4.1" = "sha256-Ij3gYrox0O813mzI+GcUa5VctR14wNKNaZ+4ynNkTmU=";
-      "mcapi-0.2.0" = "sha256-P90cQhP8kRVZ6LWYYyBWR4Qg7xS8w6+tclFMDyzo9kk=";
+      "mcapi-0.2.0" = "sha256-pJ6f26r/R9qBnbJJkDSzGi9N/BlD9seS9/vEwgSZ0I8=";
       "pathdiff-0.2.1" = "sha256-+X1afTOLIVk1AOopQwnjdobKw6P8BXEXkdZOieHW5Os=";
       "rpackwiz-0.1.0" = "sha256-pOotNPIZS/BXiJWZVECXzP1lkb/o9J1tu6G2OqyEnI8=";
     };
@@ -33,24 +32,24 @@ rustPlatform.buildRustPackage rec {
     pkg-config
   ];
 
-  buildInputs =
-    [
-      bzip2
-      zstd
-    ]
-    ++ lib.optionals stdenv.isDarwin [
-      darwin.apple_sdk.frameworks.Security
-    ];
+  buildInputs = [
+    bzip2
+    zstd
+  ] ++ lib.optionals stdenv.isDarwin [
+    darwin.apple_sdk.frameworks.CoreFoundation
+    darwin.apple_sdk.frameworks.CoreServices
+    darwin.apple_sdk.frameworks.Security
+  ];
 
   env = {
     ZSTD_SYS_USE_PKG_CONFIG = true;
   };
 
   meta = with lib; {
-    description = "Powerful Minecraft Server Manager CLI";
+    description = "Powerful Minecraft Server Manager CLI. Easily install jars (server, plugins & mods) and write config files. Docker and git support included";
     homepage = "https://github.com/ParadigmMC/mcman";
     license = licenses.gpl3Only;
-    maintainers = with maintainers; [iogamaster];
+    maintainers = with maintainers; [ iogamaster ];
     mainProgram = "mcman";
   };
 }
