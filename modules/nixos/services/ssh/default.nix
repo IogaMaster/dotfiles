c205c687822dee79dc3513512e6fb35d3a1df9e1
IogaMaster <iogamastercode@gmail.com>
2024-01-26 11:46:34 -0700

    feat: persist keys

--- modules/nixos/services/ssh/default.nix
+++ modules/nixos/services/ssh/default.nix
@@ -43,6 +43,10 @@ in {
 
     environment.persist.files = [
       "/etc/machine-id"
+      "/etc/ssh/ssh_host_ed25519_key"
+      "/etc/ssh/ssh_host_ed25519_key.pub"
+      "/etc/ssh/ssh_host_rsa_key"
+      "/etc/ssh/ssh_host_rsa_key.pub"
     ];
   };
 }
