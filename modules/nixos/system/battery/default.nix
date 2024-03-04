3f2e3ddf883e8073dba4d602f8a1c2e034cdec67
IogaMaster <iogamastercode@gmail.com>
2024-03-04 12:10:25 -0700

    feat(modules/battery): improve battery life

--- modules/nixos/system/battery/default.nix
+++ modules/nixos/system/battery/default.nix
@@ -20,9 +20,9 @@ in {
     services.tlp = {
       enable = true;
       settings = {
-        CPU_BOOST_ON_AC = 1;
+        CPU_BOOST_ON_AC = 0;
         CPU_BOOST_ON_BAT = 0;
-        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_AC = "powersave";
         CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
       };
     };
