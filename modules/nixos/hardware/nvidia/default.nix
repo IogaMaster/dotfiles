d51c5050611d0596b9dc5b9d57b55f608169e322
IogaMaster <iogamastercode@gmail.com>
2024-12-23 14:12:02 -0700

    feat(hardware/nvidia): use 565 driver

--- modules/nixos/hardware/nvidia/default.nix
+++ modules/nixos/hardware/nvidia/default.nix
@@ -18,24 +18,25 @@ in
   config = mkIf cfg.enable {
     services.xserver.videoDrivers = [ "nvidia" ];
     hardware.nvidia.modesetting.enable = true;
+    hardware.nvidia.open = false;
 
-    hardware.nvidia.package =
-      let
-        rcu_patch = pkgs.fetchpatch {
-          url = "https://github.com/gentoo/gentoo/raw/c64caf53/x11-drivers/nvidia-drivers/files/nvidia-drivers-470.223.02-gpl-pfn_valid.patch";
-          hash = "sha256-eZiQQp2S/asE7MfGvfe6dA/kdCvek9SYa/FFGp24dVg=";
-        };
-      in
-      config.boot.kernelPackages.nvidiaPackages.mkDriver {
-        version = "535.154.05";
-        sha256_64bit = "sha256-fpUGXKprgt6SYRDxSCemGXLrEsIA6GOinp+0eGbqqJg=";
-        sha256_aarch64 = "sha256-G0/GiObf/BZMkzzET8HQjdIcvCSqB1uhsinro2HLK9k=";
-        openSha256 = "sha256-wvRdHguGLxS0mR06P5Qi++pDJBCF8pJ8hr4T8O6TJIo=";
-        settingsSha256 = "sha256-9wqoDEWY4I7weWW05F4igj1Gj9wjHsREFMztfEmqm10=";
-        persistencedSha256 = "sha256-d0Q3Lk80JqkS1B54Mahu2yY/WocOqFFbZVBh+ToGhaE=";
-
-        patches = [ rcu_patch ];
-      };
+    # hardware.nvidia.package =
+    #   let
+    #     rcu_patch = pkgs.fetchpatch {
+    #       url = "https://github.com/gentoo/gentoo/raw/c64caf53/x11-drivers/nvidia-drivers/files/nvidia-drivers-470.223.02-gpl-pfn_valid.patch";
+    #       hash = "sha256-eZiQQp2S/asE7MfGvfe6dA/kdCvek9SYa/FFGp24dVg=";
+    #     };
+    #   in
+    #   config.boot.kernelPackages.nvidiaPackages.mkDriver {
+    #     version = "535.154.05";
+    #     sha256_64bit = "sha256-fpUGXKprgt6SYRDxSCemGXLrEsIA6GOinp+0eGbqqJg=";
+    #     sha256_aarch64 = "sha256-G0/GiObf/BZMkzzET8HQjdIcvCSqB1uhsinro2HLK9k=";
+    #     openSha256 = "sha256-wvRdHguGLxS0mR06P5Qi++pDJBCF8pJ8hr4T8O6TJIo=";
+    #     settingsSha256 = "sha256-9wqoDEWY4I7weWW05F4igj1Gj9wjHsREFMztfEmqm10=";
+    #     persistencedSha256 = "sha256-d0Q3Lk80JqkS1B54Mahu2yY/WocOqFFbZVBh+ToGhaE=";
+    #
+    #     patches = [ rcu_patch ];
+    #   };
 
     environment.variables = {
       CUDA_CACHE_PATH = "$XDG_CACHE_HOME/nv";
